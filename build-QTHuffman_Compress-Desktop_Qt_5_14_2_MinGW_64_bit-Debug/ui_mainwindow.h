53021adc1024bc6875f0b2a18eb9723feefd4471
1234-bai <863119860@qq.com>
2021-01-04 02:06:59 +0800

    做了一丢丢布局的修改

--- build-QTHuffman_Compress-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-QTHuffman_Compress-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -11,9 +11,13 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,8 +26,16 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    QSpacerItem *verticalSpacer_2;
+    QFrame *frame;
+    QHBoxLayout *horizontalLayout;
+    QSpacerItem *horizontalSpacer;
     QPushButton *deButton;
+    QSpacerItem *horizontalSpacer_2;
     QPushButton *pushButton_2;
+    QSpacerItem *horizontalSpacer_3;
+    QSpacerItem *verticalSpacer;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
@@ -31,15 +43,61 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(1066, 640);
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
         MainWindow->setStyleSheet(QString::fromUtf8(""));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
-        deButton = new QPushButton(centralwidget);
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalSpacer_2 = new QSpacerItem(20, 192, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer_2);
+
+        frame = new QFrame(centralwidget);
+        frame->setObjectName(QString::fromUtf8("frame"));
+        frame->setFrameShape(QFrame::StyledPanel);
+        frame->setFrameShadow(QFrame::Raised);
+        horizontalLayout = new QHBoxLayout(frame);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalSpacer = new QSpacerItem(198, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        deButton = new QPushButton(frame);
         deButton->setObjectName(QString::fromUtf8("deButton"));
-        deButton->setGeometry(QRect(210, 250, 201, 151));
-        pushButton_2 = new QPushButton(centralwidget);
+        sizePolicy.setHeightForWidth(deButton->sizePolicy().hasHeightForWidth());
+        deButton->setSizePolicy(sizePolicy);
+        deButton->setMinimumSize(QSize(200, 150));
+
+        horizontalLayout->addWidget(deButton);
+
+        horizontalSpacer_2 = new QSpacerItem(197, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_2);
+
+        pushButton_2 = new QPushButton(frame);
         pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
-        pushButton_2->setGeometry(QRect(530, 250, 201, 151));
+        sizePolicy.setHeightForWidth(pushButton_2->sizePolicy().hasHeightForWidth());
+        pushButton_2->setSizePolicy(sizePolicy);
+        pushButton_2->setMinimumSize(QSize(200, 150));
+
+        horizontalLayout->addWidget(pushButton_2);
+
+        horizontalSpacer_3 = new QSpacerItem(198, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_3);
+
+
+        verticalLayout->addWidget(frame);
+
+        verticalSpacer = new QSpacerItem(20, 192, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
         MainWindow->setCentralWidget(centralwidget);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -53,8 +111,8 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "\345\223\210\345\244\253\346\233\274\345\216\213\347\274\251\350\275\257\344\273\266", nullptr));
-        deButton->setText(QCoreApplication::translate("MainWindow", "\350\247\243\345\216\213\346\226\207\344\273\266", nullptr));
-        pushButton_2->setText(QCoreApplication::translate("MainWindow", "\345\216\213\347\274\251\346\226\207\344\273\266", nullptr));
+        deButton->setText(QCoreApplication::translate("MainWindow", "\345\216\213\347\274\251\346\226\207\344\273\266", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "\350\247\243\345\216\213\346\226\207\344\273\266", nullptr));
     } // retranslateUi
 
 };
